bcf4d58db4bcd6f02900bf4a6a9355b7020a8c4f
PaoloRobolini <nonilmiopsn@gmail.com>
2025-04-02 13:42:42 +0200

    Implementazione iniziale socket

--- Client/main.py
+++ Client/main.py
@@ -1,7 +1,15 @@
 import socket
 
 nome_utente = input("Inserisci il tuo nome: ")
+destinatario = input("Inserisci il destinatario: ")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 5000))
+server = ("127.0.0.1", 65432)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+while True:
+    messaggio = input(f"Inserisci un messaggio per {destinatario}: ")
+    if messaggio == "exit":
+        break
+    s.sendto(messaggio.encode("utf-8"), server)
+    messaggio_ricevuto, indirizzo_client = s.recvfrom(1024)
+    print(f"{destinatario}: {messaggio_ricevuto.decode('utf-8')}")
