5b706db2bc647365819fb3128a772319725a5bf9
SixMax06 <alemilamax@gmail.com>
2025-04-05 20:48:01 +0200

    Aggiornamento codice
    chat di testo funzionante con casella di input e tasto per conferma

--- test_gui/main.py
+++ test_gui/main.py
@@ -1,14 +1,24 @@
 import dearpygui.dearpygui as dpg
 
+def invia_messaggio():
+    messaggio = dpg.get_value("input_messaggio")
+    dpg.set_value("chat_testo", messaggio)
+
 if __name__ == '__main__':
     dpg.create_context()
-    dpg.create_viewport(title="Chat", width=800, height=600)
+    dpg.create_viewport(title="Chat", width=1280, height=720)
+    dpg.add_viewport_menu_bar()
 
-    with dpg.window(label="Chat", width=800, height=600, no_move=True, no_resize=True):
-        dpg.add_text("Questa è la chat")
+    with dpg.viewport_menu_bar():
+        with dpg.menu(label="Opzioni"):
+            dpg.add_menu_item(label="Attiva/Disattiva Schermo Intero", callback=dpg.toggle_viewport_fullscreen)
 
-    with dpg.window(label="Input", width=800, height=100, no_move=True, no_resize=True):
-        dpg.add_input_text()
+    with dpg.window(tag="chat", width=600, height=600, pos=(0, 19), no_move=True, no_resize=True, no_close=True, no_title_bar=True):
+        dpg.add_text(tag="chat_testo")
+
+    with dpg.window(tag="input", width=600, height=100, pos=(0, 619), no_move=True, no_resize=True, no_close=True, no_title_bar=True):
+        dpg.add_input_text(tag="input_messaggio")
+        dpg.add_button(tag="bottone_messaggio", label="Invia messaggio", callback=invia_messaggio)
 
     dpg.setup_dearpygui()
     dpg.show_viewport()
