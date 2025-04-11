bbeb5170052769c14995423e7ae84425339758cd
PaoloRobolini <nonilmiopsn@gmail.com>
2025-04-11 16:21:11 +0200

    primo tentativo di implementazione con kivy

--- Client/main.py
+++ Client/main.py
@@ -1,8 +1,14 @@
+import json
+import multiprocessing
+import socket
+import threading
+from multiprocessing import Queue
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
 from kivy.lang import Builder
-
+from utente import utente
 
 Builder.load_file("chat.kv")
 
@@ -10,22 +16,29 @@ class LoginScreen(Screen):
     def login(self):
         username = self.ids.username_input.text.strip()
         if username:
+            self.utente = utente(username)
             chat_screen = self.manager.get_screen('chat')
             chat_screen.username = username
             self.manager.current = 'chat'
-
 
 
 class ChatScreen(Screen):
     username = StringProperty("")
     chat_history = StringProperty("")
 
-    def send_message(self):
+
+    def send_message(self, socket):
         message = self.ids.message_input.text.strip()
         if message:
-            self.chat_history += f"\n{self.username}: {message}"
-            self.chat_history += f"\nBot: Ciao {self.username}, hai detto: \"{message}\""
+            self.chat_history += f"\n{self.username} > {message}"
             self.ids.message_input.text = ""
+            azione = utente.crea_azione(comando="messaggio", destinatario=..., messaggio=message)
+            s.sendto(json.dumps(azione).encode(), server)
+
+    def receive_message(self, coda: Queue):
+        ricevuto = coda.get(block=False, timeout=1)
+        if ricevuto:
+            self.chat_history += ricevuto
 
 
 class ChatApp(App):
@@ -37,4 +50,34 @@ class ChatApp(App):
 
 
 if __name__ == '__main__':
+
+    def ricevi_messaggi(coda):
+        while True:
+            try:
+                # Riceve sia i dati che l'indirizzo del mittente
+                data, addr = s.recvfrom(1024)
+                messaggio = data.decode()
+
+                # Decodifica e processa il messaggio
+                try:
+                    messaggio = json.loads(messaggio)
+                    if "mittente" in messaggio:  # Verifica che sia un messaggio valido
+                        coda.put(f"\nMessaggio da {messaggio['mittente']} > {messaggio['messaggio']}")
+
+                except json.decoder.JSONDecodeError:
+                    coda.put("Errore: messaggio non valido")
+
+            except ConnectionResetError as e:
+                coda.put(f"Errore nella ricezione: {e}")
+                pass
+
+
+    server = ("26.21.230.217", 65432)
+    coda = multiprocessing.Queue()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    thread_ricevi = threading.Thread(target=ricevi_messaggi, args=(coda,))
+    thread_ricevi.start()
+
     ChatApp().run()
